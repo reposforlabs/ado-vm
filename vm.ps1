3462e9cb36f62925cb6ae8ad88ea6dff87f63b1c
reposforlabs <160736679+reposforlabs@users.noreply.github.com>
2024-06-19 13:20:25 +0530

    Create vm.ps1

---
+++ vm.ps1
@@ -0,0 +1,23 @@
+resourcegroup="RG-VM"
+location="westus3"
+vmname="VM1"
+username="azureuser"
+userpassword='123456789aa#'
+
+az group create --name $resourcegroup --location $location
+
+az vm create --resource-group $resourcegroup `
+--name $vmname `
+--image Win2022AzureEditionCore `
+--public-ip-sku Standard `
+--admin-username $username `
+--admin-password $userpassword
+
+az vm run-command invoke -g $resourcegroup `
+-n $vmname `
+--command-id RunPowerShellScript `
+--scripts "Install-WindowsFeature -name Web-Server -IncludeManagementTools"
+
+az vm open-port --port 80 `
+--resource-group $resourcegroup `
+--name $vmname
